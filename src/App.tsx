fef4c4cb671d51607f38ad72412fe2c652b73523
Molodyk Andrei <tarstarkas00@mail.ru>
2024-08-12 19:54:22 +0700

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchApi } from "./store/githubSlice";
+import { RootState, AppDispatch } from "./store";
+
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchApi("app"));
+  }, [dispatch]);
+
+  const { data } = useSelector((state: RootState) => state.gs);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/store/githubSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const url = process.env.REACT_APP_BASE_URL;
+const token = "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";
+
+export const fetchApi = createAsyncThunk(
+  "GitHubApi/fetchApi",
+  async function (search: string, { rejectWithValue }) {
+    try {
+      const res = await fetch(
+        `https://api.github.com/search/repositories?q=${search}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+          },
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Server Error!");
+      }
+      const data = await res.json();
+      return data;
+    } catch (error: any) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+const initialState = {
+  data: [],
+  loading: false,
+  error: "",
+};
+
+const githubSlice = createSlice({
+  name: "githubapi",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchApi.pending, (state) => {
+      state.loading = true;
+      state.error = "";
+    });
+    builder.addCase(fetchApi.fulfilled, (state, action) => {
+      state.data = action.payload;
+      //   state.loading = false;
+    });
+    builder.addCase(fetchApi.rejected, (state, action) => {
+      //   state.loading = false;
+      state.error = action.payload as string;
+    });
+  },
+});
+
+export default githubSlice.reducer;
